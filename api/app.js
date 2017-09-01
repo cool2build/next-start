1cd179569e0e55eb2d6f4ac8ca990fb1d299881f
Eric Mai <enmai1988@gmail.com>
2017-08-31 22:42:18 -0700

    added sidebar and removed LRUCache

--- api/app.js
+++ api/app.js
@@ -7,43 +7,18 @@ const user = require('./user');
 const db = require('../models');
 
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dir: '.', dev, quiet: false })
+const app = next({ dev })
 const handle = app.getRequestHandler();
-const middleware = require('./middleware');
 const port = process.env.PORT || 3000;
 
 app.prepare()
   .then(() => {
     const server = express();
 
-    server.get('/', (req, res) => middleware.renderAndCache(app, req, res, '/'));
-
+    server.get('/', (req, res) => app.render(req, res, '/', req.query));
     server.get('*', handle);
-  //   let app = express();
-  //   app.server = http.createServer(app);
 
-  //   app.use(cors({
-  //     exposedHeaders: config.corsHeaders
-  //   }));
-
-  //   app.use(bodyParser.json({
-  //     limit : config.bodyLimit
-  //   }));
-
-  //   // internal middleware
-  //   //app.use(middleware({ config, db  }));
-
-  //   // api router
-  //   user(app, config, db); //This is the extra line
-
-  //   app.get('*', (req, res) => handle(req, res))
-
-  //   app.server.listen(process.env.PORT || config.port);
-
-  //   console.log(`Started on port ${app.server.address().port}`);
-  // })
-  // .catch(err => {
-  //   console.log(err.stack);
     server.listen(port, () => console.log(`listening on port ${port}`));
-  });
+  })
+  .catch(err => console.log(err));
 
